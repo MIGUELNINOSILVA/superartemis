649b50389a0c1b02e32d53071761324785b8d18e
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-07-12 09:19:01 -0500

    feat:

--- backend-node-mongo/models/Categorias.js
+++ backend-node-mongo/models/Categorias.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const categoriaShcema = mongoose.Schema({
+const categoriaSchema = mongoose.Schema({
     imagen: {
         type: String,
         required: true,
@@ -23,6 +23,33 @@ const categoriaShcema = mongoose.Schema({
 
 );
 
-const Categoria = mongoose.model("Categoria", categoriaShcema);
+const clientesSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    nombre_compannia: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    direccion: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    telefono: {
+        type: Number,
+        required: true,
+        trim: true
+    }
+},
+    {
+        timestamp: true
+    });
 
-export default Categoria;
+const Categoria = mongoose.model("Categoria", categoriaSchema);
+const Cliente = mongoose.model("Cliente", clientesSchema);
+
+export default {Categoria, Cliente};
