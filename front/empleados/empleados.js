d3806c07a179602aa0c662922b513b74f6389f1c
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-07-13 07:51:44 -0500

    feat: :zap: Agregar todas las funciones EMPLEADOS desde consumo js

---
+++ front/empleados/empleados.js
@@ -0,0 +1,129 @@
+import { getAllEmpleados } from '../js/API.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    showDataEmpleados();
+
+});
+
+async function showDataEmpleados() {
+    const data = await getAllEmpleados();
+    const bodyTable = document.getElementById('empleadoContent');
+    data.forEach((empleado, index) => {
+        const { _id, nombre, titulo, fecha_nacimiento, fecha_contratacion, direccion, ciudad, telefono } = empleado;
+        bodyTable.innerHTML += `
+        <tr>
+            <th scope="row">${index + 1}</th>
+            <td>${nombre}</td>
+            <td>${titulo}</td>
+            <td>${fecha_nacimiento}</td>
+            <td>${fecha_contratacion}</td>
+            <td>${direccion}</td>
+            <td>${ciudad}</td>
+            <td>${telefono}</td>
+            <td>
+                <button type="button" class="btn btn-warning editar" data-bs-toggle="modal" data-bs-target="#editarClienteModal" id="${_id}">Editar</button>
+                <button class="btn btn-danger eliminar" id="${_id}">Eliminar</button>
+            </td>
+        </tr>
+        `
+    });
+    // Llamar a la función para mostrar los botones de eliminación
+    showEliminarButtons();
+    sendInfoForm();
+    showDataEdit();
+}
+
+function showEliminarButtons() {
+    const botonEliminar = document.querySelectorAll('.eliminar');
+    botonEliminar.forEach(eliminar => {
+        eliminar.addEventListener('click', async () => {
+            swal({
+                title: "Estás seguro?",
+                text: "No podrás recuperar tus datos!",
+                icon: "warning",
+                buttons: [
+                    'No, cancelar!',
+                    'Sí, estoy segur@!'
+                ],
+                dangerMode: true,
+            }).then(function (isConfirm) {
+                if (isConfirm) {
+                    swal({
+                        title: 'Eliminado Correctamente!',
+                        text: 'Ya no puedes recuperar tus datos',
+                        icon: 'success'
+                    }).then(function () {
+                        deleteClientes(eliminar.id);
+                        window.location = 'clientes.html';
+                    });
+                } else {
+                    swal("Cancelado", "No has eliminado tu data", "error");
+                }
+            });
+
+        })
+    });
+}
+
+function sendInfoForm() {
+    const agregarClienteForm = document.querySelector('#agregarClienteForm');
+    const nombreClienteForm = document.querySelector('#nombreClienteForm');
+    const nombreCompaniaClienteForm = document.querySelector('#nombreCompaniaClienteForm');
+    const direccionClienteForm = document.querySelector('#direccionClienteForm');
+    const telefonoClienteForm = document.querySelector('#telefonoClienteForm');
+
+    agregarClienteForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const data = {
+            nombre: nombreClienteForm.value,
+            nombre_compannia: nombreCompaniaClienteForm.value,
+            direccion: direccionClienteForm.value,
+            telefono: telefonoClienteForm.value
+        }
+        if (insertClientes(data)) {
+            swal("Datos enviados satisfactoriamente", "¡Enviado!", "success");
+            setTimeout(() => {
+                window.location = 'clientes.html';
+            }, 2000);
+        }
+    })
+}
+
+function showDataEdit() {
+    const nombreClienteFormEdit = document.querySelector('#nombreClienteFormEdit');
+    const nombreCompaniaClienteFormEdit = document.querySelector('#nombreCompaniaClienteFormEdit');
+    const direccionClienteFormEdit = document.querySelector('#direccionClienteFormEdit');
+    const telefonoClienteFormEdit = document.querySelector('#telefonoClienteFormEdit');
+    const editarClienteModal = document.querySelector('#editarClienteModal');
+
+    const buttonEditar = document.querySelectorAll('.editar');
+
+    buttonEditar.forEach(editar => {
+        editar.addEventListener('click', async () => {
+            const id = editar.id;
+            const cliente = await getOneClientes(id);
+            console.log(cliente);
+            nombreClienteFormEdit.value = cliente.nombre;
+            nombreCompaniaClienteFormEdit.value = cliente.nombre_compannia;
+            direccionClienteFormEdit.value = cliente.direccion;
+            telefonoClienteFormEdit.value = cliente.telefono;
+            editarClienteModal.addEventListener('submit', async (e) => {
+                e.preventDefault();
+                const newObject = {
+                    nombre: nombreClienteFormEdit.value,
+                    nombre_compannia: nombreCompaniaClienteFormEdit.value,
+                    direccion: direccionClienteFormEdit.value,
+                    telefono: telefonoClienteFormEdit.value
+                }
+                console.log(newObject);
+                if (await updateClientes(id, newObject)) {
+                    swal("Datos enviados actualizados correctamente", "¡Enviado!", "success");
+                    setTimeout(() => {
+                        window.location = 'clientes.html';
+                    }, 1500);
+                }
+            })
+        })
+    });
+
+}
--- front/js/API.js
+++ front/js/API.js
@@ -1,5 +1,6 @@
 const urlCategoria = "http://localhost:5001/categorias";
 const urlCliente = "http://localhost:5001/clientes";
+const urlEmpleado = "http://localhost:5001/empleados";
 
 // Categorias API
 export async function getAllCategorias() {
@@ -136,6 +137,77 @@ export async function updateClientes(id, data) {
             body: JSON.stringify(data)
         })
         const response = await clientes.json();
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Empleados API
+export async function getAllEmpleados() {
+    try {
+        const empleados = await fetch(`${urlEmpleado}/all`);
+        return empleados.json();
+    } catch (error) {
+        console.log(error);
+        swal({
+            title: "Error 404",
+            text: "Error Conexión a la Base de datos!",
+            icon: "error",
+          });
+    }
+}
+
+export async function getOneEmpleados(id) {
+    try {
+        const empleados = await fetch(`${urlEmpleado}/${id}`);
+        return empleados.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function insertEmpleados(data) {
+    try {
+        const empleados = await fetch(`${urlEmpleado}/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        })
+        const response = await empleados.json();
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function updateEmpleados(id, data) {
+    try {
+        const empleados = await fetch(`${urlEmpleado}/upd/${id}`,{
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        const response = await empleados.json();
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function deleteEmpleados(id) {
+    try {
+        const empleados = await fetch(`${urlEmpleado}/remove/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const response = await empleados.json();
+        window.location = 'index.html';
         return response;
     } catch (error) {
         console.log(error);
