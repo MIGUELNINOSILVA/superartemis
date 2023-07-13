c5035f2c56a0ba8c87852da987d014f747a30448
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-07-13 07:39:07 -0500

    feat: :zap: Metodos HTTP funcionales BACKEND

--- backend-node-mongo/routes/empleados.routes.js
+++ backend-node-mongo/routes/empleados.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { obtenerEmpleados } from '../controllers/empleado.controllers.js';
+import { obtenerEmpleados, obtenerUnEmpleado, agregarEmpleados, borrarEmpleados, updateEmpleados } from '../controllers/empleado.controllers.js';
 
 const router = express.Router();
 
 router.get('/all', obtenerEmpleados);
-
+router.get('/:id', obtenerUnEmpleado);
+router.post('/add', agregarEmpleados);
+router.delete('/remove/:id', borrarEmpleados);
+router.patch('/upd/:id', updateEmpleados);
 
 export default router;
